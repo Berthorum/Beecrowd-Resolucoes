6a30c71f153998ffb2f864ddac95e875c80a2f98
Ravier <43036041+Berthorum@users.noreply.github.com>
2025-11-11 18:07:42 -0300

    Resolução das Questões da Categoria SQL (Da 2602 a 2611)

---
+++ categorias/sql/2610.sql
@@ -0,0 +1,1 @@
+SELECT AVG(price)::NUMERIC(10,2) AS price FROM products;
---
+++ categorias/sql/2611.sql
@@ -0,0 +1,4 @@
+SELECT movies.id, movies.name
+FROM movies LEFT JOIN genres
+ON movies.id_genres = genres.id
+WHERE genres.description = 'Action';
