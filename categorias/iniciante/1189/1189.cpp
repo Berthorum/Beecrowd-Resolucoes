45326aea9858fa19bb208f5bca12af6eb10e14dc
Ravier <43036041+Berthorum@users.noreply.github.com>
2025-12-18 19:24:01 -0300

    Resolução dos Exercícios 1188 a 1190 em C++

---
+++ categorias/iniciante/1189/1189.cpp
@@ -0,0 +1,29 @@
+#include <iomanip>
+#include <iostream>
+
+int main() {
+
+    char op;
+    int cont = 0;;
+    double m[12][12], res = 0.0;
+
+    std::cin >> op;
+
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            std::cin >> m[i][j];
+            if ((j < i) && (j + i < 11)) {
+                res += m[i][j];
+                cont++;
+            }
+        }
+    }
+
+    if (op == 'M') {
+        res /= cont;
+    }
+
+    std::cout << std::fixed << std::setprecision(1)  << res << std::endl;
+
+    return 0;
+}
